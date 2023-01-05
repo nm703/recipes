95603242049e497dc89f0a0cc17182395bcf524b
Nevena Markovic <nevenamarkovic703@gmail.com>
2023-01-05 23:24:30 +0100

    Adding_recipes_using_spoonacular

---
+++ src/components/Veggie.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
+
+function Veggie() {
+
+    const [veggie, setVeggie] = useState([]);
+
+     //zove fju cim se pokrene strana, only run it when the component gets mounted
+     useEffect(() => {
+        getVeggie();
+    }, []);
+
+    
+
+    // async because we're waiting for the data
+    const getVeggie = async () => {
+
+        const check = localStorage.getItem("veggie");
+       
+
+        if(check){
+            // getting the data back, prebaci iz string-a opet u array item
+            setVeggie(JSON.parse(check));
+        } else {
+            const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`
+            );
+            const data = await api.json();
+            
+
+            // making an array of data into a string and saving it that way
+            localStorage.setItem("veggie", JSON.stringify(data.recipes));
+
+            setVeggie(data.recipes);
+        }
+
+    };
+  return (
+    <div>
+    <Wrapper>
+        <h3>Veggie Picks</h3>
+
+        <Splide options={{
+            perPage: 3,
+            arrows:false,
+            pagination:false,
+            drag:'free',
+            gap:"5rem",
+        }}>
+            {veggie.map((recipe) => {
+                return (
+                    <SplideSlide key={recipe.id}>
+                        <Card>
+                            <p>{recipe.title}</p>
+                            <img src={recipe.image} alt={recipe.title} />
+                            <Gradient/>
+                        </Card>
+                    </SplideSlide>
+                );
+            })}
+        </Splide>
+    </Wrapper>
+
+</div>
+  )
+}
+const Wrapper = styled.div`
+margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+min-height:25rem;
+border-radius: 2rem;
+overflow:hidden;
+position:relative;
+
+img {
+    border-radius:2rem;
+    position:absolute;
+    left: 0;
+    width:100%;
+    height:100%;
+    object-fit:cover;
+
+
+}
+
+p{
+   position:absolute;
+   z-index:10;
+   left:50%;
+   bottom:0%;
+   color:white;
+   width:100%;
+   transform:translate(-50%, 0%);
+   text-align:center;
+   font-size:1rem;
+   display:flex;
+   jusitfy-content: center;
+   font-weight:600;
+   align-items:center;
+
+
+}
+
+`;
+
+const Gradient = styled.div`
+z-index:3;
+position:absolute;
+width:100%;
+height:100%;
+background:linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+`;
+export default Veggie
